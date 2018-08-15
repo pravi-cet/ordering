589a48ab31e5f4c4818c867ee2e1f91bec46867c
pravi-cet <ravi.cet@gmail.com>
2018-08-15 12:14:56 -0400

    Message for no ingredients

--- src/components/B/B.js
+++ src/components/B/B.js
@@ -3,12 +3,18 @@ import classes from './B.css';
 import BIngredient from './Ingredient/Ingredient';
 
 const B = (props) => {
-  const arrIngredients = Object.keys(props.ingredients)
+  let arrIngredients = Object.keys(props.ingredients)
                             .map(igKey =>{
                                 return [...Array(props.ingredients[igKey])].map((_, i) =>
                                 <BIngredient key={igKey + i} type={igKey} />
                               )
-                            });
+                            })
+                            .reduce((arr, el) => { //arr - prev value, el new value
+                              return arr.concat(el);
+                            }, [] ); //initial value is blank array
+  if (arrIngredients.length === 0){
+    arrIngredients = <p>Please start adding ingredients</p>
+  }
   return (
     <div className={classes.B}>
       <BIngredient type="bread-top" />
--- src/containers/BBuilder/BBuilder.js
+++ src/containers/BBuilder/BBuilder.js
@@ -4,10 +4,10 @@ import B from '../../components/B/B';
 class BBuilder extends Component{
   state = {
     ingredients : {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
   render() {
